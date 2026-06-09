dca264fa71c82f504a6b6c2efd2eb6840e7da728
Rafael Felipe <rafaguiar2005@gmail.com>
2026-06-09 16:32:25 -0400

    feat: estrutura inicial do repositório\n\n- Adicionada Atividade01 com projeto ASP.NET MVC\n- Configurado .gitignore para .NET\n- Adicionado README.md com estrutura das atividades

---
+++ Atividade01/Atividades LPV II/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Atividades_LPV_II.Models;
+
+namespace Atividades_LPV_II.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ README.md
@@ -0,0 +1,19 @@
+# Atividades LPV II
+
+Repositório para armazenar as atividades da disciplina **Linguagem de Programação Visual II**.
+
+## Estrutura
+
+Cada pasta representa uma atividade diferente:
+
+- **Atividade01** - Projeto ASP.NET MVC inicial
+
+## Como usar
+
+1. Navegue até a pasta da atividade desejada
+2. Abra a solução `.sln` no Visual Studio ou Rider
+3. Execute o projeto normalmente
+
+---
+
+*Curso de LPV II*
